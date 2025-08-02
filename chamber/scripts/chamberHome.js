7386771830501036fdb5690fec8656ead1412d41
odilebo <odilebo279@gmail.com>
2025-08-03 00:41:04 +0200

    changes

--- chamber/scripts/chamberHome.js
+++ chamber/scripts/chamberHome.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
     const toggleButton = document.getElementById("menu-toggle");
-    const navMenu = document.getElementById("nav-menu");
+    const navMenu = document.querySelector(".navigation");
+
   
     toggleButton.addEventListener("click", () => {
       navMenu.classList.toggle("active");
