3e56d006d1477e828f6ecec856eb56cba5c75ce0
odilebo <odilebo279@gmail.com>
2025-08-09 21:04:40 +0200

    final join

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -18,3 +18,26 @@ window.onclick = e => {
     e.target.style.display = 'none';
   }
 };
+
+const full = document.querySelector("#currentyear");
+const day = document.querySelector("#lastModified");
+
+
+const today = new Date();
+let lastMod = new Date(document.lastModified);
+
+full.innerHTML = `Today is <span class="highlight">${new Intl.DateTimeFormat(
+	"en-US",
+	{
+		dateStyle: "full"
+	}
+).format(today)}</span>`;
+
+day.innerHTML = `Last Modified:  <span class="highlight">${new Intl.DateTimeFormat(
+	"en-US",
+	{
+		dateStyle: "full"
+	}
+).format(lastMod)}</span>`;
+
+full.innerHTML = ` <span class="highlight">${today.getFullYear()}</span>`;
